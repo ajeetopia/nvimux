bb3ed72fdb51268eb350cfa011853f9a5f17c404
Ajeet Kumar <kumarajeet08@gmail.com>
2024-08-10 19:57:11 +0000

    Fix plugin.lua

--- nvim/lua/plugins/myplugins.lua
+++ nvim/lua/plugins/myplugins.lua
@@ -9,129 +9,129 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- add gruvbox
-  --{ "ellisonleao/gruvbox.nvim" },
-  
-  {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
-  }
+    -- add gruvbox
+        --{ "ellisonleao/gruvbox.nvim" },
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
     },
-  },
 
-  -- override nvim-cmp and add cmp-emoji
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
-
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
+    -- Configure LazyVim to load gruvbox
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "tokyonight",
+        },
     },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
+
+    -- override nvim-cmp and add cmp-emoji
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = { "hrsh7th/cmp-emoji" },
+        ---@param opts cmp.ConfigSchema
+            opts = function(_, opts)
+            table.insert(opts.sources, { name = "emoji" })
+            end,
     },
-  },
 
-  -- Enable clangd for C++
-  { import = "lazyvim.plugins.extras.lang.clangd" },
-
-  -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "awk",
-        "bash",
-        "c",
-        "cpp",
-        "css",
-        "csv",
-        "cuda",
-        "devicetree",
-        "diff",
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "glsl",
-        "html",
-        "http",
-        "javascript",
-        "json",
-        "kconfig",
-        "linkerscript",
-        "lua",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "objc",
-        "objdump",
-        "passwd",
-        "pem",
-        "printf",
-        "proto",
-        "python",
-        "query",
-        "regex",
-        "rst",
-        "ssh_config",
-        "strace",
-        "todotxt",
-        "udev",
-        "vim",
-        "xml",
-        "yaml",
-      },
+    -- change some telescope options and a keymap to browse plugin files
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            -- add a keymap to browse plugin files
+                -- stylua: ignore
+                {
+                    "<leader>fp",
+                    function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+                    desc = "Find Plugin File",
+                },
+        },
+        -- change some options
+            opts = {
+                defaults = {
+                    layout_strategy = "horizontal",
+                    layout_config = { prompt_position = "top" },
+                    sorting_strategy = "ascending",
+                    winblend = 0,
+                },
+            },
     },
-  },
 
-  -- use mini.starter instead of alpha
-  --{ import = "lazyvim.plugins.extras.ui.mini-starter" },
-  -- tmux integration
-  {
+    -- Enable clangd for C++
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+
+    -- add more treesitter parsers
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "awk",
+                "bash",
+                "c",
+                "cpp",
+                "css",
+                "csv",
+                "cuda",
+                "devicetree",
+                "diff",
+                "git_config",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "gitignore",
+                "glsl",
+                "html",
+                "http",
+                "javascript",
+                "json",
+                "kconfig",
+                "linkerscript",
+                "lua",
+                "make",
+                "markdown",
+                "markdown_inline",
+                "objc",
+                "objdump",
+                "passwd",
+                "pem",
+                "printf",
+                "proto",
+                "python",
+                "query",
+                "regex",
+                "rst",
+                "ssh_config",
+                "strace",
+                "todotxt",
+                "udev",
+                "vim",
+                "xml",
+                "yaml",
+            },
+        },
+    },
+
+    -- use mini.starter instead of alpha
+        --{ import = "lazyvim.plugins.extras.ui.mini-starter" },
+    -- tmux integration
+    {
         "alexghergh/nvim-tmux-navigation",
         config = function()
             require("nvim-tmux-navigation").setup({ disable_when_zoomed = true })
-        end
-  },
-
-  -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shfmt",
-      },
+            end
     },
-  },
+
+    -- add any tools you want to have installed below
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "stylua",
+                "shfmt",
+            },
+        },
+    },
 }
